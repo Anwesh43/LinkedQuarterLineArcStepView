3508209bcc7f9b60992e48deb1d9cddce77a8288
Anwesh43 <anweshthecool0@gmail.com>
2019-01-01 09:57:01 +0530

    adding Int and Float extension methods

--- quaterlinearcstepview/src/main/java/com/anwesh/uiprojects/quaterlinearcstepview/QuarterLineArcStepView.kt
+++ quaterlinearcstepview/src/main/java/com/anwesh/uiprojects/quaterlinearcstepview/QuarterLineArcStepView.kt
@@ -21,3 +21,19 @@ val sizeFactor : Float = 2.8f
 val strokeFactor : Int = 90
 val rotDeg : Float = 360f
 val color : Int = Color.parseColor("#4CAF50")
+
+fun Int.inverse() : Float = 1f / this
+
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Float.scaleFactor()  : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.inverse() + scaleFactor() * b.inverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Int.scaleX() : Float = 1f - 2 * (this % 2)
+
+fun Int.scaleY() : Float = 1f - 2 * (this / 2)
