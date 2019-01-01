c06c345d984f5bc946c5dec82662f97db08e21b0
Anwesh43 <anweshthecool0@gmail.com>
2019-01-01 10:42:38 +0530

    adding QLASNode class

--- quaterlinearcstepview/src/main/java/com/anwesh/uiprojects/quaterlinearcstepview/QuarterLineArcStepView.kt
+++ quaterlinearcstepview/src/main/java/com/anwesh/uiprojects/quaterlinearcstepview/QuarterLineArcStepView.kt
@@ -130,4 +130,48 @@ class QuarterLineArcStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class QLASNode(var i : Int, val state : State = State())  {
+
+        private var next : QLASNode? = null
+        private var prev : QLASNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = QLASNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawQLASNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit){
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : QLASNode {
+            var curr : QLASNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
