5bacbfefbdd424fd39574cfb8b79891fb0d47c73
Anwesh43 <anweshthecool0@gmail.com>
2019-01-01 09:52:13 +0530

    adding constants and importing libraries

--- quaterlinearcstepview/src/main/java/com/anwesh/uiprojects/quaterlinearcstepview/QuarterLineArcStepView.kt
+++ quaterlinearcstepview/src/main/java/com/anwesh/uiprojects/quaterlinearcstepview/QuarterLineArcStepView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.quaterlinearcstepview
 /**
  * Created by anweshmishra on 01/01/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val arcs : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.8f
+val strokeFactor : Int = 90
+val rotDeg : Float = 360f
+val color : Int = Color.parseColor("#4CAF50")
